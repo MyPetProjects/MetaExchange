beff3830edd3663f5ab118cbb76023fb3f033653
Valentine Loshakov <valentine.loshakov@gmail.com>
2022-10-03 14:14:44 +0300

    Created .NET Core API project and added it to the solution

---
+++ MetaExchangeAPI/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetaExchangeAPI.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("buy_btc")]
+        public IActionResult BuyBtc(decimal amount)
+        {
+            return Problem("Endpoint not impelemnted");
+        }
+
+        [HttpPost("sell_btc")]
+        public IActionResult SellBtc(decimal amount)
+        {
+            return Problem("Endpoint not impelemnted");
+        }
+    }
+}
